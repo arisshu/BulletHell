d04ac7133d74119bdc0bed7b1596827b857dd154
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2022-04-18 17:26:41 -0700

    Player scripts/parallax implementation

--- character/player/player.gd
+++ character/player/player.gd
@@ -1,2 +1,62 @@
 extends Area2D
+class_name Player
 
+var plBullet := preload("res://Bullet/PlayerBullet.tscn")
+
+onready var firingPositions := $FiringPositions
+onready var fireDelayTimer := $FireDelayTimer
+onready var invulTimer := $InvulTimer
+
+
+# use colon to specify type
+export var speed:float = 100.0
+export var fireDelay:float = 0.1
+export var life:int = 3
+export var invulTime:float = 0.5
+# := is like above except
+# godot infers the type on
+# the right side
+var vel:= Vector2(0,0)
+
+func _ready():
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
+		fireDelayTimer.start(fireDelay)
+		for child in firingPositions.get_children():
+			var bullet := plBullet.instance()
+			bullet.global_position = child.global_position
+			get_tree().current_scene.add_child(bullet)
+
+
+func _physics_process(delta):
+	var dirVec := Vector2(0,0)
+	
+	if Input.is_action_pressed("moveLeft"):
+		dirVec.x = -1
+	elif Input.is_action_pressed("moveRight"):
+		dirVec.x = 1
+	if Input.is_action_pressed("moveUp"):
+		dirVec.y = -1
+	elif Input.is_action_pressed("moveDown"):
+		dirVec.y = 1
+	
+	vel = dirVec.normalized() * speed
+	position += vel * delta	
+	
+	# Make sure that the player is within the screen.
+	var viewRect := get_viewport_rect()
+	position.x = clamp(position.x, 0, viewRect.size.x)
+	position.y = clamp(position.y, 0, viewRect.size.y)
+	
+func damage(amount: int):
+	if !invulTimer.is_stopped():
+		return
+	
+	invulTimer.start(invulTime)
+	life -= amount
+
+	if life <= 0:
+		queue_free()
+	
