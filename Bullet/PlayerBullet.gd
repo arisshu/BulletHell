ffc0a03ac45f18d9d6f8a154f4e750d96993c80d
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2022-04-18 17:29:22 -0700

    Textures

---
+++ Bullet/PlayerBullet.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export var speed:float = 500
+
+func _physics_process(delta):
+	position.y -= speed * delta
+	
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_Bullet_area_entered(area):
+	pass
