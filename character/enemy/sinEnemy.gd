918e75415dbca4ba94b4d9688360fb8026f11c75
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2022-04-21 17:49:47 -0700

    Scene added

---
+++ character/enemy/sinEnemy.gd
@@ -0,0 +1,13 @@
+extends Enemy
+
+# Limits for the frequency and amplitude
+export (float, 1, 1000) var frequency = 5
+export (float, 1000) var amplitude = 150
+export (int, -1, 1) var direction = 1
+
+var time = 0
+
+func _physics_process(delta):
+	time += delta
+	var movement = cos(time * frequency)*amplitude 
+	position.x += movement * delta * direction
