26e155273fb239d773379004dd448f36215c20a8
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2022-04-27 16:52:33 -0700

    Enemy Hit animation v.01

--- character/enemy/sinEnemy.gd
+++ character/enemy/sinEnemy.gd
@@ -7,8 +7,19 @@ export (float, 1000) var amplitude = 300
 export (int, -1, 1) var direction = 1
 
 var time = 0
+onready var anim = $AnimationPlayer
 
 func _physics_process(delta):
 	time += delta
 	var movement = cos(time * frequency)*amplitude 
 	position.x += movement * delta * direction
+
+func damage(amount: int):
+	health -= amount
+	if health <= 0:
+		var effect := plExplosion.instance()
+		effect.global_position = global_position
+		get_tree().current_scene.add_child(effect)
+		
+		GlobalVar.enemyOnCurrentScreen.erase(self.name)
+		queue_free()
