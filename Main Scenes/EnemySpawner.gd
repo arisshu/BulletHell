183c6c5cffa044c01c7981b24ac3fcbc5cd84e2e
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2022-04-27 15:42:10 -0700

    Working EnemySpawner

--- Main Scenes/EnemySpawner.gd
+++ Main Scenes/EnemySpawner.gd
@@ -6,42 +6,70 @@ var preloadedEnemies := [
 	preload("res://character/enemy/EnemySine.tscn"),
 	preload("res://character/enemy/ShootEnemy.tscn")
 ]
+
 onready var spawnTimer := $SpawnTimer
 
-export (int, 0, 4) var enemy = 1
-export var spawnerSpeed = 100
-export var enemySpeed = 300
-export var enemyHealth = 5
-export var amount = 1
-export (float, 1, 1000) var sineFrequency = 5
-export (float, 1000) var sineAmplitude = 300
-export (int, -1, 1) var sineDirection = 1 
-export var spawnInterval: float = 1
-
-
-
-
-func _physics_process(delta):
-	position.y += spawnerSpeed * delta
-
-func _on_VisibilityNotifier2D_screen_entered():
-	spawnerSpeed = 0
-	spawnTimer.start(spawnInterval)
+var dict
+var pos: int = 0
+var arrayLength: int = 0
+var enemyPos: Vector2 = Vector2(0,0)
+var time: float = 1
+var type: int = 0
+var amount: int = 1
+var delay: float = 0.3
+var enemyLines: int = 1
+var offsetX: float = 0.0
+var offsetY: float = 0.0
+	
+func _ready():
+	var file = File.new()
+	file.open("res://LevelFiles/Level1.json", File.READ)
+	var text =  file.get_as_text()
+	dict = JSON.parse(text)
+	file.close()
+	arrayLength = dict.result["SpawnEnemyArray"].size() - 1
+	spawn_enemies(dict)
+	
+func _on_SpawnTimer_timeout():
+	place_enemies(type, enemyPos)
+	var linesPlaceHolder = enemyLines
+	var positions = enemyPos
+	
+	while linesPlaceHolder > 0:
+		print("Line #", linesPlaceHolder)
+		place_enemies(type, positions)
+		positions += Vector2(offsetX, offsetY)
+		linesPlaceHolder -= 1
+		
+	amount -= 1
+	if amount == 0:
+		if ! pos >= arrayLength:
+			pos += 1
+			spawn_enemies(dict)
+		else: 
+			print("I AM HIT")
+			spawnTimer.stop()
+			return
+	
+	spawnTimer.start(delay)
 	
 
-func _on_SpawnTimer_timeout():
-	var preloadEnemy = preloadedEnemies[enemy]
+func place_enemies(type, position):
+	var preloadEnemy = preloadedEnemies[type]
 	var myEnemy: Enemy = preloadEnemy.instance()
-	myEnemy.speed = enemySpeed
-	myEnemy.health = enemyHealth
-	if enemy == 2:
-		myEnemy.direction = sineDirection
-		myEnemy.amplitude = sineAmplitude
-		myEnemy.frequency = sineFrequency
-	if amount == 0:
-		queue_free()
-		
-	myEnemy.position = Vector2(global_position.x, 0)
+	myEnemy.position = position
 	get_tree().current_scene.add_child(myEnemy)
-	amount -= 1
-	spawnTimer.start(spawnInterval)
+
+
+func spawn_enemies(enemyArray):
+	time = float(enemyArray.result["SpawnEnemyArray"][pos]["Time"])
+	enemyPos = Vector2(float(enemyArray.result["SpawnEnemyArray"][pos]["LocationX"]), float(enemyArray.result["SpawnEnemyArray"][pos]["LocationY"]))
+	type = int(enemyArray.result["SpawnEnemyArray"][pos]["Type"])
+	amount = int(enemyArray.result["SpawnEnemyArray"][pos]["Amount"])
+	delay = float(enemyArray.result["SpawnEnemyArray"][pos]["Delay"])
+	enemyLines = int(enemyArray.result["SpawnEnemyArray"][pos]["EnemyLines"])
+	offsetX = float(enemyArray.result["SpawnEnemyArray"][pos]["OffsetX"])
+	offsetY = float(enemyArray.result["SpawnEnemyArray"][pos]["OffsetY"])
+	print("setting time", time)
+	spawnTimer.start(time)
+	
