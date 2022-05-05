116dca12c1135922a2649e188b2f581a0c35f740
Calvin La <luminoslayer147@gmail.com>
2022-05-05 14:35:41 -0700

    Remove extra unnecessary horizontalEnemy scene for better application

--- character/enemy/HorizontalEnemyShoot.gd
+++ character/enemy/HorizontalEnemyShoot.gd
@@ -3,11 +3,25 @@ extends Enemy
 
 onready var fireTimer := $FireTimer
 
+var startPosX
+
 #export var fireRate := 2
 #export var fireChance := 5
 
+func _ready():
+	startPosX = self.position.x
+	if (startPosX > (get_viewport().size.x/2)):
+		$Sprite.set_global_rotation_degrees(-90)
+		$FiringPositions.set_global_rotation_degrees(-90)
+		$FiringPositions.set_position(Vector2(0,-15))
+	print(startPosX)
+	print(get_viewport().size.y)
+
 func _process(delta):
-	position.x += speed * delta
+	if (startPosX <= (get_viewport().size.x/2)):
+		position.x += speed * delta
+	else:
+		position.x -= speed * delta
 	position.y -= speed * delta
 	
 	if fireTimer.is_stopped():
